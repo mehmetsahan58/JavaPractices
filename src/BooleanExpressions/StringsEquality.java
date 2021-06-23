88b26b8bb4907cd29142785f6f659414487f271a
ms <mehmetsahan58@gmail.com>
2021-06-22 19:00:33 -0500

    String Equality / Practices

---
+++ src/BooleanExpressions/StringsEquality.java
@@ -0,0 +1,24 @@
+package BooleanExpressions;
+
+public class StringsEquality {
+
+    public static void main (String args[]) {
+
+        String A = "hello";
+        String B = new String ("hello");
+
+        System.out.println ("A vs B");
+        System.out.println();
+
+        System.out.println ("If you use ==: " + (A == B));
+        System.out.println ("if you use equals: " + (A.equals(B)));
+
+        String C = "hello";
+        System.out.println ("A vs C");
+        System.out.println();
+
+        System.out.println ("If you use == :" + (A == C));
+        System.out.println ("If you use equals :" + (A.equals(C)));
+
+    }
+}
